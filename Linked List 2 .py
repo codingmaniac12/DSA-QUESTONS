515040bdef7f716181695bb6bfbe4cc9f42e0fd1
MckinellGreen <khandelwalshriyansh1208@gmail.com>
2022-10-01 18:03:33 +0530

    Added Linked List Insertion tail

---
+++ Linked List 2 .py
@@ -0,0 +1,25 @@
+class Node:
+    def __init__(self,key):
+        self.key=key
+        self.next=None
+def insertend(a,head):
+    if head==None:
+        return Node(a)
+    temp=Node(a)
+    curr=head
+    while curr.next!=None:
+        curr=curr.next
+    curr.next=temp
+    return head
+head=None
+n=int(input())
+for i in range(n):
+    a=int(input()) # key to be inserted at head
+    head=insertend(a,head)
+def printlist(head):
+    curr=head
+    while curr!=None:
+        print(curr.key,end=' ')
+        curr=curr.next
+printlist(head)
+    
